8588d04c234fc9854a50c18be6f73a007ded7e48
Orimitsu Serizawa <weave_light-00@yahoo.co.jp>
2023-06-11 18:07:25 +0900

    Sequence was modified.

--- Seq/my_sequence_pkg.sv
+++ Seq/my_sequence_pkg.sv
@@ -14,13 +14,29 @@ package my_sequence_pkg;
     int data;
 
     virtual task body();
+
+      get_ral_CONFIG_AAA ();
+      read_reg_CONFIG_AAA();
+      write_field_CONFIG_AAA();
+      read_field_CONFIG_AAA ();
+
+    endtask
+
+    virtual function void get_ral_CONFIG_AAA();
       msg = "- Read RAL Values of CONFIG_AAA ---";
       msg = {msg, $sformatf(": 0x%8H",this.reg_ralget("CONFIG_AAA") )};
       `uvm_info(get_name(), msg, UVM_MEDIUM)
-      this.reg_read ("CONFIG_AAA",  data);
+    endfunction
+
+    virtual task read_reg_CONFIG_AAA();
+      this.reg_read ("CONFIG_AAA", data);
       msg = "- Read REG Values of CONFIG_AAA ---";
       msg = {msg, $sformatf(" : 0x%8H",data )};
       `uvm_info(get_name(), msg, UVM_MEDIUM)
+
+    endtask
+
+    virtual task write_field_CONFIG_AAA();
     //this.reg_write("CONFIG_AAA",   100);
       this.fld_ralset("CONFIG_AAA.param_0",  9);
       this.fld_ralset("CONFIG_AAA.param_1",  9);
@@ -30,7 +46,9 @@ package my_sequence_pkg;
       this.fld_ralset("CONFIG_AAA.param_5",  2);
       this.fld_ralset("CONFIG_AAA.param_6",  2);
       this.reg_update("CONFIG_AAA");
+    endtask
 
+    virtual task read_field_CONFIG_AAA();
     //this.reg_read ("CONFIG_AAA", rdata);
       this.reg_mirror("CONFIG_AAA");
       msg = "\n- Read Values of CONFIG_AAA ---";
@@ -42,7 +60,6 @@ package my_sequence_pkg;
       msg = {msg, $sformatf("\n |- param_5 : %0d", this.fld_ralget("CONFIG_AAA.param_5"))};
       msg = {msg, $sformatf("\n |- param_6 : %0d", this.fld_ralget("CONFIG_AAA.param_6"))};
       `uvm_info(get_name(), msg, UVM_MEDIUM)
-
     endtask
 
   endclass
@@ -56,21 +73,21 @@ package my_sequence_pkg;
     endfunction
 
     virtual task body();
-      start_seq1();
-      start_seq2();
+      ral_seq();
+      c_seq  ();
       this.start_c_sequence();
     endtask
 
-    virtual task start_seq1();
-      example_ral_sequence seq1;
-      seq1 = example_ral_sequence::type_id::create("seq1");
-      seq1.start_on_ral(this.model, this);
+    virtual task ral_seq();
+      example_ral_sequence ral_seq;
+      ral_seq = example_ral_sequence::type_id::create("ral_seq");
+      ral_seq.start_on_ral(this.model, this);
     endtask
 
-    virtual task start_seq2();
-      my_uvm_pkg::my_c_ral_sequence seq2;
-      seq2 = my_uvm_pkg::my_c_ral_sequence::type_id::create("seq2");
-      seq2.start_on_ral(this.model, this);
+    virtual task c_seq();
+      my_uvm_pkg::my_c_ral_sequence c_seq;
+      c_seq = my_uvm_pkg::my_c_ral_sequence::type_id::create("c_seq");
+      c_seq.start_on_ral(this.model, this);
     endtask
 
   endclass
